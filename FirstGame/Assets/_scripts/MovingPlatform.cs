88c7964e002ad6b215c0618f0724c64e6ae70989
Mattias <mattias_lindholm41@hotmail.com>
2018-06-19 13:19:01 +0200

    Fixat en vector3 calculation distance
    Kunde inte hitta dina modeler till bann byggstenarna (dom du visade igår
    med övredelen gräs och under delen dirt)

--- FirstGame/Assets/_scripts/MovingPlatform.cs
+++ FirstGame/Assets/_scripts/MovingPlatform.cs
@@ -35,10 +35,14 @@ public class MovingPlatform : MonoBehaviour {
     {
         if( wheretoGoList.Length > 0)
         {            
-                if (Mathf.Abs(transform.position.x - goal.x) < 1 && Mathf.Abs(transform.position.y - goal.y) < 1 && Mathf.Abs(transform.position.z - goal.z) < 1)
+                //if (Mathf.Abs(transform.position.x - goal.x) < 1 && Mathf.Abs(transform.position.y - goal.y) < 1 && Mathf.Abs(transform.position.z - goal.z) < 1)
+                
+                if(VectorDistanceCallculator(transform.position, goal))
                 {
-                number++;
-                transform.position = goal;
+                    transform.position = goal;
+                    number++;
+                
+                
 
                 if (number > wheretoGoList.Length - 1)
                 {
@@ -57,6 +61,18 @@ public class MovingPlatform : MonoBehaviour {
                 
                 Debug.Log(transform.position + "______" + goal);
          }                
+    }
+
+
+    private bool VectorDistanceCallculator(Vector3 Vector_1, Vector3 Vector_2)
+    {
+        float distance = (Mathf.Sqrt(Mathf.Pow((Vector_1.x - Vector_2.x), 2) + Mathf.Pow((Vector_1.y - Vector_2.y), 2) + Mathf.Pow((Vector_1.z - Vector_2.z), 2)));
+        if(distance < 0.5 && distance > -0.5)
+        {
+            return true;
+        }
+
+        return false;
     }
 
    
